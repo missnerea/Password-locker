6b7e4942d0b0e406f04b18af19c97207620b7eca
‟ElaineNerea” <ela.amish@Gmail.com>
2018-01-19 11:36:34 +0300

    Add variables for user class

---
+++ user.py
@@ -0,0 +1,9 @@
+Class User :
+   """
+   Class that generates new instances of users.
+   """
+   def __init__(self,first_name,last_name,email,password):
+       self.first_name= first_name
+       self.last_name= last_name
+       self.number= number
+       self.password= password

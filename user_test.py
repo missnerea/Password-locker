c7e31ab04f7c1f87394953c54af89a720cfec90a
‟ElaineNerea” <ela.amish@Gmail.com>
2018-01-19 13:03:26 +0300

    Add code to pass tests

--- user_test.py
+++ user_test.py
@@ -14,6 +14,12 @@ class TestUser(unittest.TestCase):
          '''
          self.new_user = User("Elaine","Nerea","elaine@n.com","0712345")
 
+     def tearDown(self):
+        '''
+        tearDown method that does clean up after each test case has run.
+        '''
+        User.user_list = []
+
      def test_init(self):
          '''
          test_init test case to test if object is iniialized properly
@@ -32,15 +38,16 @@ class TestUser(unittest.TestCase):
          self.new_user.save_user()
          self.assertEqual(len(User.user_list),1)
 
-    def test_save_multipe_user(self):
+     def test_save_multipe_user(self):
         '''
         Test save_multiple_user to check if we can save multiple user objects to our user list.
         '''
         self.new_user.save_user()
         test_user = User("Test","user","test@user.com","0712345")
         test_user.save_user()
-        self.assertEqual(len(User.user_list),2) 
+        self.assertEqual(len(User.user_list),2)
 
+     
 
 if __name__ == '__main__':
     unittest.main()
